dbcf59ef2bff21d5b0b8c9decd0d14929b3237f3
lchen104 <lchen@zdmedia.com>
2023-08-31 10:22:13 -0400

    Updated routes and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,34 @@
+
+
+//@ desc    Register new user
+//@route    POST /api/users
+//@access   PUBLIC
 const registerUser = (req, res) => {
     res.json({
         message: 'Register User'
     })
 }
 
+//@ desc    Authenticate a user
+//@route    POST /api/users/login
+//@access   PUBLIC
+const loginUser = (req, res) => {
+    res.json({
+        message: 'Login User'
+    })
+}
+
+//@ desc    Get user data
+//@route    GET /api/users/me
+//@access   PUBLIC
+const getMe = (req, res) => {
+    res.json({
+        message: 'Display user data'
+    })
+}
+
 module.exports = {
     registerUser,
+    loginUser,
+    getMe
 }
